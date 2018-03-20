98ebe843d80008a43ca7b81734aeeb17d8c7401d
bodrus <maxshop2010@gmail.com>
2018-03-20 19:06:55 +0300

    first commit

---
+++ sicp/test1/01.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+;!!!!
+(require rackunit)
+
+(define (solution a b) (+ a b))
+          
+(printf "Результат - ") (solution 10 5)
+
+
+(check-equal? (solution 2 2 ) 4)
+(check-equal? (solution 5 5 ) 10 (printf "test 1.3 passed"))
+
