588e2c6e69c0bfcb978c42abb127888b44d2a099
bodrus <maxshop2010@gmail.com>
2018-04-23 23:38:59 +0300

    add 2.23

--- doc/chapter02/solutions/2.17/2.17.rkt
+++ doc/chapter02/solutions/2.17/2.17.rkt
@@ -4,13 +4,13 @@
 
 ; Процедура получения последнего элемента списка
 (define (last-pair list1)
-	(if (null? list1)
+	(if (null? (cdr list1))
 						 list1
 						 (last-pair (cdr list1))))
 
 ; Пробный вызов						 
-(define test-list (list 23 72 149 34))
-(last-pair test-list)
+;;; (define test-list (list 23 72 149 34))
+;;; (last-pair test-list)
 
 ; Экспорт процедуры
 (provide last-pair)
