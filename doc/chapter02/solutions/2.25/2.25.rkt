013741bd0fcfecc6018896429ef93d497c06bca0
bodrus <maxshop2010@gmail.com>
2018-04-24 15:02:25 +0300

    add chenge

--- doc/chapter02/solutions/2.25/2.25.rkt
+++ doc/chapter02/solutions/2.25/2.25.rkt
@@ -15,3 +15,8 @@
 (define li (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
 
 (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr li))))))))))))
+; // или так 
+
+(cadadr (cadadr (cadadr li)))
+
+
